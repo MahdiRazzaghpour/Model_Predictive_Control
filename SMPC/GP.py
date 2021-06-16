646eda0ba4963c8ec02b2482410eb7c9e9e2b478
Mahdi Razzaghpour <ma121036@ucf.edu>
2021-06-15 21:50:03 -0400

    Update GP.py

--- SMPC/GP.py
+++ SMPC/GP.py
@@ -11,7 +11,7 @@ raw_data = loadmat('speed.mat')
 speed = raw_data['Speed']
 speed = np.array(speed)
 
-t=10
+t=50
 
 # ----------------------------------------------------------------------
 # First the noiseless case
@@ -28,7 +28,11 @@ real_speed = speed[t:t+100]
 
 # Instantiate a Gaussian Process model
 kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=0)
+gp = GaussianProcessRegressor( kernel              = kernel,
+                    alpha               = 0.01,
+                    copy_X_train        = True,
+                    optimizer           = "fmin_l_bfgs_b",
+                    n_restarts_optimizer= 50)
 
 # Fit to data using Maximum Likelihood Estimation of the parameters
 gp.fit(X, y)
@@ -45,7 +49,7 @@ plt.plot(x, y_pred, 'b-', label='Prediction')
 plt.fill(np.concatenate([x, x[::-1]]),
          np.concatenate([y_pred - 1.9600 * sigma,
                         (y_pred + 1.9600 * sigma)[::-1]]),
-         alpha=.5, fc='b', ec='None', label='95% confidence interval')
+         alpha=0.5, fc='b', ec='None', label='95% confidence interval')
 plt.xlabel('$T$')
 plt.ylabel('$Speed$')
 # plt.ylim(-10, 20)
